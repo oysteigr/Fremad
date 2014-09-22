2a7d9cfda2063582a21d276245c0244a5d2721f0
Øystein W. Grændsen <oysteigr@gmail.com>
2014-09-22 22:13:13 +0200

    Added working REST with config

--- src/main/java/fremad/gui/Front.java
+++ src/main/java/fremad/gui/Front.java
@@ -4,21 +4,12 @@ package fremad.gui;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-/**
- * This is the root path for our restful api service
- * In the web.xml file, we specified that /api/* need to be in the URL to
- * get to this class.
- * 
- * We are versioning the class in the URL path.  This is the first version v1.
- * Example how to get to the root of this api resource:
- * http://localhost:7001/com.youtube.rest/api/v1/status
- * 
- * @author 308tube
- *
- */
-//@Path("/v1/front/*")
-public class Front {
 
+
+
+@Path("/front")
+public class Front {
+	
 	private static final String api_version = "00.01.00"; //version of the api
 	
 	/**
@@ -27,6 +18,7 @@ public class Front {
 	 * 
 	 * @return String - Title of the api
 	 */
+	
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String returnTitle() {
