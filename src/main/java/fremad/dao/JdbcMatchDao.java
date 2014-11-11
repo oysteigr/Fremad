9c94b18b2677f5a54a1c203e05f919bd213d1948
Øystein Grændsen <oysteigr@gmail.com>
2014-11-11 21:40:56 +0100

    changed MatchList

--- src/main/java/fremad/dao/JdbcMatchDao.java
+++ src/main/java/fremad/dao/JdbcMatchDao.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
+import fremad.domain.MatchListObject;
 import fremad.domain.MatchObject;
 import fremad.dao.SqlTablesConstants;
 
--- src/main/java/fremad/domain/MatchListObject.java
+++ src/main/java/fremad/domain/MatchListObject.java
@@ -6,14 +6,5 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement(name = "MatchListObject")
 public class MatchListObject extends AbstractListWrapper<MatchObject>{
 	
-	private TeamObject teamObject;
-
-	public TeamObject getTeamObject() {
-		return teamObject;
-	}
-
-	public void setTeamObject(TeamObject teamObject) {
-		this.teamObject = teamObject;
-	}
 
 }
