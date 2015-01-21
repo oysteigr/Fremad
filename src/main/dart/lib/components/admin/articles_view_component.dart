6b4a3d5c4a1da33ec216d5cba7399e1bb1b63059
Øystein Grændsen <oysteigr@gmail.com>
2015-01-21 22:03:13 +0100

    Added article base

--- src/main/dart/lib/components/admin/articles_view_component.dart
+++ src/main/dart/lib/components/admin/articles_view_component.dart
@@ -6,8 +6,42 @@ part of fremad;
     useShadowDom: false
 )
 class ShowAdminArticlesComponent {
-  void create() {
-    html.window.console.info("Is in ShowAdminArticlesComponent");
+  final Http _http;
+  bool teamsLoaded;
+  bool playersLoaded;
+  bool isEditing = false;
+  bool isAdding = false;
+  int selectedPlayer = -1;
+  TeamList teamListObject;
+  List<Team> teamList;
+  PlayerList playerListObject;
+  List<Player> playerList;
+  Article articlePlayer = new Article(-1, -1, new DateTime.now(), "", "", "", "", "",false);
+  
+  ShowAdminArticlesComponent(this._http){
+    loadArticles();
+    loadUsers();
   }
-
+  
+  void loadArticles() {
+    html.window.console.info("Is in loadTeams");
+    teamsLoaded = false;
+    _http.get('rest/team/getTeams.json')
+      .then((HttpResponse response) {
+        print(response);
+        teamListObject = new TeamList.fromJson(response.data);
+        teamList = teamListObject.teamList;
+        teamsLoaded = true;
+        html.window.console.info("Success on loading teams");
+      })
+      .catchError((e) {
+        print(e);
+        teamsLoaded = false;
+        html.window.console.info("Could not load rest/team/getTeams.json");
+      });
+  } 
+  
+  void loadUsers(){
+    
+  }
 }
