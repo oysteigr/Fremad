83cd4b28c876f3b6e7927cf60da30b31fe255c0f
Halvor Eggen <halvore@gmail.com>
2015-02-28 17:00:44 +0100

    Removed unused imports

--- src/main/java/fremad/dao/JdbcMatchDao.java
+++ src/main/java/fremad/dao/JdbcMatchDao.java
@@ -12,7 +12,6 @@ import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
-import fremad.domain.LeagueObject;
 import fremad.domain.MatchObject;
 import fremad.domain.list.MatchListObject;
 
